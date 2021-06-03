36470e6a0962df0084cbf6e91926c3262999cde2
parzivale <zeus@theolivers.org>
2021-06-03 16:18:23 +0200

    finished final code

--- Code/Final-sketches/irrigation-system/irrigation-system.ino
+++ Code/Final-sketches/irrigation-system/irrigation-system.ino
@@ -1,48 +1,74 @@
 #include <SPI.h>
 #include <SD.h>
-#include <Time.h>
 #include <dht.h>
 
-float temperature, humidity = 10.0;
-int moisture, nowTime = 10;
+#define dht_apin A0
+#define moisture_apin A0
+#define Moisture_min 10
+#define Moisture_max 550
+
+int moisture;
 File myFile;
+dht DHT;
 
 void setup()
 {
     Serial.begin(9600);
+    delay(500);
     if (!SD.begin(10))
     {
         Serial.println("initialization failed!");
-        while (1)
-            ;
+        while (1);
     }
-
     if (!SD.exists("data.csv"))
     {
         myFile = SD.open("data.csv", FILE_WRITE);
-        if(myFile){
-           myFile.print("temperature,humidiity,moisture");
+        if (myFile)
+        {
+            myFile.print("Temperature,Humidity,Moisture");
         }
     }
     else
     {
         myFile = SD.open("data.csv", FILE_WRITE);
     }
+}
+
+void loop()
+{
+
     if (myFile)
     {
-        Serial.print("Writing data...");
 
-        myFile.print(temperature);
+        Serial.print("Reading DHT device... ");
+        DHT.read11(dht_apin);
+        Serial.println("Read DHT device succsefully");
+
+        Serial.print("Reading Moisture Sensor...");
+        int output_value = analogRead(moisture_apin);
+        moisture = map(output_value, Moisture_max, Moisture_min, 0, 100);
+        Serial.println("Sensor read Correctly");
+
+        Serial.print("Current humidity = ");
+        Serial.print(DHT.humidity);
+        Serial.print("%  ");
+        Serial.print("temperature = ");
+        Serial.println(DHT.temperature);
+
+        Serial.print("Current moisture = ");
+        Serial.print(moisture);
+        Serial.println("%");
+
+        Serial.print("Writing data... ");
+
+        myFile.print(DHT.temperature);
         myFile.print(",");
-        myFile.print(humidity);
+        myFile.print(DHT.humidity);
         myFile.print(",");
         myFile.print(moisture);
 
         Serial.println("closing file");
         myFile.close();
     }
-}
-
-void loop()
-{
+    delay(1000);
 }
